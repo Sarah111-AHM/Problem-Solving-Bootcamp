a10fbbf4c01971318eeb73a8da2971ac7628967d
Sarah Abumandil <114596306+Sarah111-AHM@users.noreply.github.com>
2026-04-27 13:54:59 +0300

    Create Week 01 — Many A+B Problems (AtCoder - abc288_a).cpp

---
+++ V Judge problems/Answers/Week 01 — Many A+B Problems (AtCoder - abc288_a).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        long long a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+    return 0;
+}
