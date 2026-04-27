e0ec60a2e3d73cb725cc331e43a085bc74cf87ed
Sarah Abumandil <114596306+Sarah111-AHM@users.noreply.github.com>
2026-04-27 13:55:37 +0300

    Create Week 02 — Bit++ (CodeForces - 282A).cpp

---
+++ V Judge problems/Answers/Week 02 — Bit++ (CodeForces - 282A).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n, x = 0;
+    cin >> n;
+    while (n--) {
+        string s;
+        cin >> s;
+        if (s == "++X" || s == "X++") x++;
+        else x--;
+    }
+    cout << x << "\n";
+    return 0;
+}
